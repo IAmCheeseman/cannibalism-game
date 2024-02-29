09e5fc05e6906ecbf58d4e7a0824c9996dfea9a4
unknown <xanderswan32@gmail.com>
2024-02-29 10:40:25 -0500

    Unbuggified cursor

--- cursor.lua
+++ cursor.lua
@@ -9,9 +9,12 @@ function Cursor:init()
   love.mouse.setVisible(false)
 end
 
-function Cursor:draw()
-  local mx, my = core.viewport.getMousePosition("default")
+function Cursor:gui()
+  love.graphics.setColor(1, 1, 1)
+  local mx, my = core.viewport.getMousePosition("gui")
   self.sprite:draw(math.floor(mx), math.floor(my))
 end
+
+core.events.gui:connect(world, Cursor, "gui")
 
 return Cursor
