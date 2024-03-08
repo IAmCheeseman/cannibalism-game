260028ff0b0d89104d5254e4d521379938cca6f4
Xander Swan <xanderswan32@gmail.com>
2024-03-08 17:42:09 -0500

    okay test over

--- forest.lua
+++ forest.lua
@@ -68,7 +68,6 @@ local function open(enemyCount)
     collision:destroy()
   end
   collisions = {}
-  -- test
 
   deepGrassLayer = core.TileLayer(worldSize, worldSize)
   deepGrassLayer:addTileSet(deepGrassTs)
